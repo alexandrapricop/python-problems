31f49b1047c64ba6b8c0d3b9bdcb40e6d15258ce
Alexandra Pricop <alexandrapricop@alexandra.local>
2023-09-01 00:36:20 +0300

    Solve 58.Length of last word

---
+++ leetcode/58-length-of-last-word.py
@@ -0,0 +1,14 @@
+#Given a string s consisting of words and spaces, return the length of the last word in the string.
+# A word is a maximal substring consisting of non-space characters only.
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        last_length = 0
+        for c in range(len(s)-1, -1, -1):
+            if s[c] != " ":
+                last_length = last_length + 1
+            else:
+                if last_length > 0:
+                    return last_length
+        
+        return last_length
